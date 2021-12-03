cd7ca84356ee3002ca47cbc6ed747b98d366ccc5
nec4 <42926839+nec4@users.noreply.github.com>
2021-12-04 00:00:49 +0100

    Update utils.py

--- torchmdnet/utils.py
+++ torchmdnet/utils.py
@@ -4,6 +4,19 @@ import numpy as np
 import torch
 from os.path import dirname, join, exists
 
+def is_notebook():
+    from IPython import get_ipython
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
 if is_notebook():
     from tqdm.notebook import tqdm
 else:
